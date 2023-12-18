9994360e12ceae558d2931c39a55660ad98746b5
skyneton <67633420+skyneton@users.noreply.github.com>
2023-12-18 10:26:46 +0900

    UUIDTypeAdapter fromString convert.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "net.mpoisv.ban"
-version = "1.0-SNAPSHOT"
+version = "1.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
--- src/main/java/net/mpoisv/ban/utils/UUIDConverter.java
+++ src/main/java/net/mpoisv/ban/utils/UUIDConverter.java
@@ -41,6 +41,22 @@ public class UUIDConverter {
     }
 
     public static UUID getUUIDFromUUID(String uuid) {
-        return UUIDTypeAdapter.fromString(uuid);
+        return fromString(uuid);
+    }
+
+    public static UUID fromString(String uuid) {
+        if(uuid.contains("-")) return UUID.fromString(uuid);
+        if(uuid.length() != 32) return null;
+        var builder = new StringBuilder();
+        builder.append(uuid.substring(8))
+                .append('-')
+                .append(uuid, 8, 12)
+                .append('-')
+                .append(uuid, 12, 16)
+                .append('-')
+                .append(uuid, 16, 20)
+                .append('-')
+                .append(uuid, 20, 36);
+        return UUID.fromString(builder.toString());
     }
 }
